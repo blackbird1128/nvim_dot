b48d90727c3f6cf1c0c7f6674717bf4ddef771d6
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2023-10-01 12:31:37 +0200

    add lualine and switch leap by flash

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,25 @@
+require('lualine').setup {
+    options = {
+        icons_enabled = false,
+        theme = 'seoul256',
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '|', right = '|' }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {"os.date('%H:%M')", 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+
+}
