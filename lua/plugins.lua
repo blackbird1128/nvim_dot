485e78351e7796599b5a3c006c58a76d68c711f4
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2023-10-07 14:30:28 +0200

    Add rust as a lazy plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,34 +2,25 @@ return {
 
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        dependencies = { 'nvim-lua/plenary.nvim',   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },},
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim',   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }}
   },
   {
-        "folke/which-key.nvim",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            plugins = {
-                registers = false,
-            }
-        }
+      "folke/which-key.nvim",
+      init = function()
+          vim.o.timeout = true
+          vim.o.timeoutlen = 500
+      end,
+      opts = {
+          plugins = { registers = true}
+      }
   },
   {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        ---@type Flash.Config
-        opts = {},
-        -- stylua: ignore
-        keys = {
-            { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-        },
+      "aserowy/tmux.nvim",
+      lazy = true,
+      init = function ()
+          return require("tmux").setup()
+      end
   },
   {
       'stevearc/oil.nvim',
@@ -38,21 +29,38 @@ return {
       dependencies = { "nvim-tree/nvim-web-devicons" },
   },
   {
-        'nvim-lualine/lualine.nvim',
+      'nvim-lualine/lualine.nvim',
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+    event = {"BufReadPre", "BufNewFile"},
+  },
   {'tpope/vim-fugitive', cmd="Git"},
-  {'lewis6991/gitsigns.nvim' },
-  {'junegunn/vim-peekaboo' },
+  {'lewis6991/gitsigns.nvim'},
   'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   {'neovim/nvim-lspconfig'},
-  {"windwp/nvim-autopairs", lazy=true},
+  {"windwp/nvim-autopairs", event = "BufRead"},
   {   "github/copilot.vim",
   build = ":Copilot setup",
   event = "BufRead",
     },
     {   'simrat39/rust-tools.nvim', ft = 'rust'},
-    { 'rust-lang/rust.vim', ft = 'rust' , config = function() vim.g.rustfmt_autosave = 1 end},
+    { 'rust-lang/rust.vim', ft = 'rust' , config = function() 
+            vim.g.rustfmt_autosave = 1
+            local rt = require('rust-tools')
+            lspconfig.rust_analyzer.setup({
+                checkOnSave = {
+                    command = "clippy"
+                },
+                on_attach = function(_, bufnr)
+                    -- Hover actions
+                    vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions,
+                    { buffer = bufnr })
+                    -- Code action groups
+                end,
+                cmd = {"rustup", "run", "stable", "rust-analyzer"},
+            })
+        end
+    },
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
@@ -63,13 +71,14 @@ return {
             {'williamboman/mason-lspconfig.nvim'}, -- Optional
             -- Autocompletion
             {'hrsh7th/cmp-nvim-lsp-signature-help'}, -- Optional
-            {"hrsh7th/cmp-path"}, -- Optional
+            {"hrsh7th/cmp-path", lazy=true}, -- Optional
             {'hrsh7th/nvim-cmp'},     -- Required
             {"saadparwaiz1/cmp_luasnip"},
             {'hrsh7th/cmp-nvim-lsp'}, -- Required
             {
                 "L3MON4D3/LuaSnip",
+                lazy=true
+            }
         }
-    }
     }
 }
