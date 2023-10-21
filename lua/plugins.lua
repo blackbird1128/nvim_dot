8dc5ac9c6351e54314544f1b1927cf7c94e30f30
alex <=>
2023-10-21 19:57:47 +0200

    Move heavy plugins to after initial UI load

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
 return {
-
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
   {
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim',   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }}
@@ -9,7 +9,7 @@ return {
       "folke/which-key.nvim",
       init = function()
           vim.o.timeout = true
-          vim.o.timeoutlen = 500
+          vim.o.timeoutlen = 50
       end,
       opts = {
           plugins = { registers = true}
@@ -24,61 +24,40 @@ return {
   },
   {
       'stevearc/oil.nvim',
-      opts = {},
       -- Optional dependencies
       dependencies = { "nvim-tree/nvim-web-devicons" },
   },
   {
       'nvim-lualine/lualine.nvim',
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-    event = {"BufReadPre", "BufNewFile"},
+   {
+       "github/copilot.vim",
+       event = "InsertEnter",
   },
+  {
+      'simrat39/rust-tools.nvim', ft = 'rust'
+  },
+  {
+      'rust-lang/rust.vim', ft = 'rust', config = function()
+          local lspconfig =  require("lspconfig")
+          vim.g.rustfmt_autosave = 1
+          local rt = require('rust-tools')
+          lspconfig.rust_analyzer.setup({
+              checkOnSave = {
+                  command = "clippy"
+              },
+              on_attach = function(_, bufnr)
+                  -- Hover actions
+                  vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions,
+                  { buffer = bufnr })
+                  -- Code action groups
+              end,
+              cmd = {"rustup", "run", "stable", "rust-analyzer"},
+          })
+      end
+  },
+
   {'tpope/vim-fugitive', cmd="Git"},
-  {'lewis6991/gitsigns.nvim'},
-  'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-  {'neovim/nvim-lspconfig'},
-  {"windwp/nvim-autopairs", event = "BufRead"},
-  {   "github/copilot.vim",
-  build = ":Copilot setup",
-  event = "BufRead",
-    },
-    {   'simrat39/rust-tools.nvim', ft = 'rust'},
-    { 'rust-lang/rust.vim', ft = 'rust' , config = function() 
-            vim.g.rustfmt_autosave = 1
-            local rt = require('rust-tools')
-            lspconfig.rust_analyzer.setup({
-                checkOnSave = {
-                    command = "clippy"
-                },
-                on_attach = function(_, bufnr)
-                    -- Hover actions
-                    vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions,
-                    { buffer = bufnr })
-                    -- Code action groups
-                end,
-                cmd = {"rustup", "run", "stable", "rust-analyzer"},
-            })
-        end
-    },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim', cmd="Mason"},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-            -- Autocompletion
-            {'hrsh7th/cmp-nvim-lsp-signature-help'}, -- Optional
-            {"hrsh7th/cmp-path", lazy=true}, -- Optional
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {"saadparwaiz1/cmp_luasnip"},
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {
-                "L3MON4D3/LuaSnip",
-                lazy=true
-            }
-        }
-    }
+  {'lewis6991/gitsigns.nvim',event = {"BufReadPre", "BufNewFile"}},
+
 }
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,53 @@
+return  {
+    {'neovim/nvim-lspconfig',event="BufAdd",
+    dependencies = {
+        {'williamboman/mason.nvim', cmd="Mason"},           -- Optional
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    },
+    config = function()
+        local lspconfig = require('lspconfig')
+        local lsp_defaults = lspconfig.util.default_config
+
+        lsp_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lsp_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+        )
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = {buffer = event.buf}
+
+
+                local nmap = function(keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
+                    end
+                    vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc })
+                end
+
+                nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                nmap("gd",function()  vim.lsp.buf.definition()end, '[G]oto [D]efinition')
+                nmap("K",function() vim.lsp.buf.hover() end, '[K]ind')
+                nmap("<leader>vd",function() vim.diagnostic.open_float() end, '[V]iew [D]iagnostics')
+                nmap("<leader>rn", function ()  vim.lsp.buf.rename()  end, '[R]ename')
+                nmap("<leader>ca", function () vim.lsp.buf.code_action() end, '[C]ode [A]ction')
+                nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+            end
+        })
+
+        local default_setup = function(server)
+            lspconfig[server].setup({})
+        end
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = {},
+            handlers = {default_setup},
+        })
+
+    end
+}
+}
