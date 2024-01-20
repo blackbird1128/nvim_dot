8ea23a4f04afd11dbeb8b97fbdfcafd05cd2fb56
alex <=>
2024-01-20 15:18:02 +0100

    Moidfy config to support why3 and small changes

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,10 @@
+
+local tbl = {}
+tbl['disable_parentheses_indenting'] =  true
+tbl['closed_paren_align_last_line'] =  false
+tbl['searchpair_timeout'] =  500
+tbl['continue'] =  'shiftwidth()'
+tbl['open_paren'] =  'shiftwidth()'
+tbl['nested_paren'] =  'shiftwidth()'
+
+vim.g.python_indent = tbl
