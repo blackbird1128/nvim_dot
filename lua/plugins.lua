d13f0f194953e4256f21a9c78ba319ada3b47e88
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2024-07-09 19:02:51 +0200

    add some more commits

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,6 +44,40 @@ return {
       opts = {}
   },
   {
+    "mcauley-penney/tidy.nvim",
+    init = function()
+     vim.keymap.set('n', "<leader>tt", require("tidy").toggle, {})
+     vim.keymap.set('n', "<leader>tr", require("tidy").run, {})
+     end
+  },
+  {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  }
+  }
+  ,{
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+  {
+      "HiPhish/rainbow-delimiters.nvim",
+  },
+  {
       "aserowy/tmux.nvim",
       lazy = true,
       init = function ()
@@ -57,6 +91,12 @@ return {
   },
   {
       'nvim-lualine/lualine.nvim',
+  },
+  {
+      'brenoprata10/nvim-highlight-colors',
+      config = function()
+          require('nvim-highlight-colors').setup({})
+      end
   },
   -- { "github/copilot.vim" ,event="InsertEnter"},
   {
