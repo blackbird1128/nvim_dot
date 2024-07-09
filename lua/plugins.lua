7dfc8646b783d5c89bd406442a57165ebb8ef9d2
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2024-07-09 19:04:46 +0200

    remove old rust config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -98,29 +98,6 @@ return {
           require('nvim-highlight-colors').setup({})
       end
   },
-  -- { "github/copilot.vim" ,event="InsertEnter"},
-  {
-      'simrat39/rust-tools.nvim', ft = 'rust'
-  },
-  {
-      'rust-lang/rust.vim', ft = 'rust', config = function()
-          local lspconfig =  require("lspconfig")
-          vim.g.rustfmt_autosave = 1
-          local rt = require('rust-tools')
-          lspconfig.rust_analyzer.setup({
-              checkOnSave = {
-                  command = "clippy"
-              },
-              on_attach = function(_, bufnr)
-                  -- Hover actions
-                  vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions,
-                  { buffer = bufnr })
-                  -- Code action groups
-              end,
-              cmd = {"rustup", "run", "stable", "rust-analyzer"},
-          })
-      end
-  },
   {'tpope/vim-fugitive', cmd="Git"},
   {'tpope/vim-dispatch', cmd={"Make","Start"}},
   {'lewis6991/gitsigns.nvim',event = {"BufReadPre", "BufNewFile"}},
